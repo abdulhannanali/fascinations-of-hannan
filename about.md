225a1a0f074c75a365dce7b7c8168b5cd16e314c
Hannan Ali <abdulhannanali@outlook.com>
2017-01-25 20:40:39 +0500

    init

---
+++ about.md
@@ -0,0 +1,8 @@
+---
+layout: page
+title: About Hannan
+permalink: /about/
+tags: about
+---
+
+<!--Write about content here-->
---
+++ scripts/deploy-staging.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+    echo "Skipping deploy; just doing a build."
+    exit 0
+fi
+
+# Deployment to firebase for master branch
+if [ "$TRAVIS_BRANCH" == "master" ]; then
+    echo "Deploying to firebase in case of master too"
+    firebase deploy --token "$FIREBASE_TOKEN"
+fi
+
+# Deploy each branch to staging, a mirror of master will be deployed on surge,
+# cos it's already being deployed on firebase too
+# Create an automated staging environment on one website, where we can select 
+# all the staging environments
+surge --domain "hannan-fascinations-""$TRAVIS_BRANCH"".surge.sh" --project ./_site
