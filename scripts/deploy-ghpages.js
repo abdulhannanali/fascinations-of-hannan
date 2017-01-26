cfda6fecea549e4eddb2beac7f7574dbd810fff2
Travis Hannan <travis@noreply.hannanali.tech>
2017-01-26 20:18:59 +0500

    Commit to gh-pages TravisCI :tada:

---
+++ scripts/deploy-ghpages.js
@@ -0,0 +1,27 @@
+const ghpages = require('gh-pages')
+const path = require('path')
+
+const GITHUB_TOKEN = process.env.GITHUB_TOKEN || '3e647f716ec932ec969cebee40f0d16360dfcc58'
+const REPO_URL = 'https://github.com/abdulhannanali/fascinations-of-hannan/'
+
+const githubUrl = generateGithubUrl(REPO_URL, GITHUB_TOKEN)
+const ghpagesOption = {
+    branch: 'gh-pages',
+    message: 'Commit to gh-pages TravisCI :tada:',
+    user: {
+        name: "Travis Hannan",
+        email: "travis@noreply.hannanali.tech"
+    },
+    repo: githubUrl
+}
+
+
+ghpages.publish(path.join(__dirname, '../_site'), options, function (error) {
+    if (error) {
+        console.error('Error occured while deploying to ghpages')
+        console.error(error)
+        process.exit(1)
+    } else {
+        console.log('Deployed  to gh-pages successfully')
+    }
+})
