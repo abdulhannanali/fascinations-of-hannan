6e2635a9c8d471d97b867fc9c208a36cfac823a7
Travis Hannan <travis@noreply.hannanali.tech>
2017-01-26 20:47:43 +0500

    Commit to gh-pages TravisCI :tada:

--- scripts/deploy-ghpages.js
+++ scripts/deploy-ghpages.js
@@ -5,16 +5,24 @@ const GITHUB_TOKEN = process.env.GITHUB_TOKEN || '3e647f716ec932ec969cebee40f0d1
 const REPO_URL = 'https://github.com/abdulhannanali/fascinations-of-hannan/'
 
 const githubUrl = generateGithubUrl(REPO_URL, GITHUB_TOKEN)
-const ghpagesOption = {
+const options = {
     branch: 'gh-pages',
     message: 'Commit to gh-pages TravisCI :tada:',
     user: {
         name: "Travis Hannan",
         email: "travis@noreply.hannanali.tech"
     },
-    repo: githubUrl
+    repo: githubUrl,
+    logger: function (message) {
+        if (message.match(GITHUB_TOKEN)) {
+            message = message.replace(GITHUB_TOKEN, '{SECURE_TOKEN}')
+        }
+        console.log('Ghpages log ' + message)
+    }
 }
 
+console.log('Cleaning the GhPages Cache, going to do this everytime')
+ghpages.clean()
 
 ghpages.publish(path.join(__dirname, '../_site'), options, function (error) {
     if (error) {
@@ -25,3 +33,7 @@ ghpages.publish(path.join(__dirname, '../_site'), options, function (error) {
         console.log('Deployed  to gh-pages successfully')
     }
 })
+
+function generateGithubUrl (repoUrl, github_token) {
+    return repoUrl.replace('https://', `https://${github_token}@`)
+}
