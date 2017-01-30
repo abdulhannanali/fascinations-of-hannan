f9faeb81f2d1a41b1ee4909e1ce48d4d41b47692
Travis Hannan <travis@noreply.hannanali.tech>
2017-01-30 04:01:58 +0000

    Commit to gh-pages TravisCI :tada:

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -ev
 
+node -v
+
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
     echo "Skipping deploy; just doing a build."
     exit 0
@@ -9,8 +11,14 @@ fi
 # Deployment to firebase for master branch
 if [ "$TRAVIS_BRANCH" == "master" ]; then
     echo "Deploying to firebase in case of master too"
-    node ./scripts/deploy-ghpages.js
     firebase deploy --token "$FIREBASE_TOKEN"
+
+    # Purge the cache if the site is successfully deployed to production
+    # We don't control other domains so we leave the purging details on them
+    node --harmony_async_await --harmony ./scripts/purger/index.js
+    
+    # deploying to Github Pages the master version, in case we need it any time
+    node ./scripts/deploy-ghpages.js
 fi
 
 # Deploy each branch to staging, a mirror of master will be deployed on surge,
@@ -18,5 +26,3 @@ fi
 # Create an automated staging environment on one website, where we can select 
 # all the staging environments
 surge --domain "hannan-fascinations-""$TRAVIS_BRANCH"".surge.sh" --project ./_site
-
-node --harmony_async_await ./scripts/purger/index.js
